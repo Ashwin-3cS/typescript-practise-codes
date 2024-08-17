eb4c0115022ceab3282d7476303db48b50feec5a
Ashwin-3cS <flashashwin2004@gmail.com>
2024-08-17 21:14:44 +0530

    objects in ts

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,54 @@ anyArray.push('ashwin');
 console.log('Any type array', anyArray);
 console.log(numbers, 'numbers Araay');
 let user = [10, 'Ash'];
-user.push('ash');
 console.log('user', user);
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+let move = Direction.Up;
+console.log(move);
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["Success"] = 200] = "Success";
+    StatusCode[StatusCode["NotFound"] = 404] = "NotFound";
+    StatusCode[StatusCode["ServerError"] = 500] = "ServerError";
+})(StatusCode || (StatusCode = {}));
+let code = StatusCode.NotFound;
+console.log(code);
+var Color;
+(function (Color) {
+    Color["Red"] = "RED";
+    Color["Green"] = "GREEN";
+    Color["Blue"] = "BLUE";
+})(Color || (Color = {}));
+let color = Color.Green;
+console.log(color);
+let directionName = Direction[0];
+console.log(directionName);
+function moveInDirection(direction) {
+    if (direction == 3) {
+        console.log("Turn left 3 inches");
+    }
+}
+moveInDirection(Direction.Left);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+calculateTax(10000);
+let employee = {
+    id: 1,
+    name: 'Ash',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+employee.retire(new Date());
+console.log(employee);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -114,5 +114,22 @@ function calculateTax (income: number, taxYear = 2022): number{  // if the taxYe
 
 calculateTax(10_000);
   
+// Objects in TS
+
+let employee: {
+  readonly id: number,
+  name: string,
+  retire: (date: Date) => void
+  } = {
+  id: 1,
+  name: 'Ash',
+  retire: (date:Date) => {
+    console.log(date);
+  }
+}
+
+employee.retire(new Date());
+
+console.log(employee);
 
   
